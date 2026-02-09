c0f1c72a8102cb3a450b63ddfcdfe30d7ab42d98
Garibaldi Cavalheiro <itgaribaldic@gmail.com>
2026-02-09 15:11:42 -0300

    Atualizar inicializacao da aplicacao

--- app/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.core.models.user import User
-from app.core.models.client import Client
-from app.core.models.product import Product
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.core.logger_config import logger
 from app.api.routes.health import router as health_router
@@ -6,17 +7,23 @@ from app.api.routes.users import router as users_router
 from app.api.routes.auth import router as auth_router
 from app.api.routes.clients import router as clients_router
 from app.api.routes.products import router as products_router
+from app.api.routes.orders import router as orders_router
 
-
-# Log de inicialização da aplicação
 logger.info(f"Aplicação {settings.app_name} iniciada no ambiente {settings.environment}")
 
-# Cria instância da aplicação FastAPI
 app = FastAPI(title=settings.app_name)
 
-# Registra routers na aplicação
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # liberar apenas em desenvolvimento
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(health_router)
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(clients_router)
 app.include_router(products_router)
+app.include_router(orders_router)
